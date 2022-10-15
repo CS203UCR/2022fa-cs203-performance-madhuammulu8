c007a802cdd3c8a5104a787567a20b2db572d75e
madhuammulu8 <madhuammulu8@gmail.com>
2022-10-14 22:57:21 -0700

    Yay! I finished the second assinment!

--- sum.cpp
+++ sum.cpp
@@ -34,7 +34,10 @@ uint64_t sum_of_locations_solution(uint64_t *search_space, uint32_t search_space
     std::unordered_set<uint64_t> mtset;  
     for (uint64_t i = 0; i < search_space_size; i++)
     {
+        if(mtset.find(search_space[i])==mtset.end())
+        {
             mtset.insert(search_space[i]);
+        }
     }
 		for(uint32_t i = 0; i < query_count; i++) {
 			if (mtset.find(queries[i]) != mtset.end()) {
